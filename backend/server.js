1bcd4de92adc37f6ca866997b39192e4b6966347
Emaad Qazi <qaziemaad@gmail.com>
2025-06-01 17:07:05 -0400

    Organizing file structure

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    res.send("You got 20 notes");
-});
+app.use("/api/notes", notesRoutes);
+
+
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
-})
+});
