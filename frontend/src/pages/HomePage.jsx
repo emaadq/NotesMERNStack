06c25dc26798f62768121c388ef29569d706d3f4
Emaad Qazi <qaziemaad@gmail.com>
2025-06-10 08:02:51 -0400

    Added ratelimiting feature

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,33 @@
 import Navbar from "../components/Navbar";
+import { useState } from "react";
+import { useEffect } from "react";
+import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
+
 
 const HomePage = () => {
-    return <div className="min-h-screen">
+    const [isRateLimited, setIsRateLimited] = useState(true);
+    const [notes, setNotes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                const res = await axios.post("http://localhost:5001/api/notes");
+                console.log(res.data)
+            }
+            catch (error) {
+                console.log("Error fetching notes");
+            }
+        };
+        fetchNotes();
+    }, []);
+
+    return  (
+    <div className="min-h-screen">
          < Navbar /> 
-    </div>;
+         {isRateLimited && <RateLimitedUI/>}
+    </div>
+    );
 };
 export default HomePage;
