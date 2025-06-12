bb4ee295c3a538d45f77277133f85cac0b2784da
Emaad Qazi <qaziemaad@gmail.com>
2025-06-12 07:16:13 -0400

    Added CORS and implementing notecard front end

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,24 @@
+// Package Imports
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Route imports 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001; // If this is undefined, we get 5001
 
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
 app.use(express.json()); // middleware: parses JSON bodies: req.body
 app.use(rateLimiter); // checks rate limiting
 
@@ -18,6 +28,11 @@ app.use(rateLimiter); // checks rate limiting
 // });
 
 app.use("/api/notes", notesRoutes);
+
+// Debug route to test backend
+app.get("/test", (req, res) => {
+    res.json({ message: "Backend is working!" });
+});
 
 // Load database THEN start server
 connectDB().then(() => {
